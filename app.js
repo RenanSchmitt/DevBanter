65f2bf1bfd22a1005d808069a35790097fd39a8e
RenanSchmitt <renanschmitt.ti@gmail.com>
2019-05-20 15:58:41 -0300

    tanananadd

--- app.js
+++ app.js
@@ -3,13 +3,13 @@ var app = express();
 
 app.set('view engine', 'ejs')
 
+app.get('/pauper', function(req, res){
+    res.render("formato/pauper");
+})
 app.get('/', function(req, res){
     res.send("<html><body> Portal de noticias </body></html>");
 })
 
-app.get('/tecnologia', function(req, res){
-    res.send("<html><body> Noticias de tecnologia </body></html>");
-})
 
 app.listen(3000, function(){
     console.log("Server is online on port 3000")
