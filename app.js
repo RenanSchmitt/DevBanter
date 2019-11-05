e365be2afed9a27934c190382185ab7a1d5c3cda
Renan Schimitt <c051461@yara.com>
2019-11-05 10:13:01 -0300

    Add base de projeto

--- app.js
+++ app.js
@@ -1,13 +1,22 @@
-var app = require('./config/server')
+const app = require('./config/server')
+const con = require('./config/db')
 app.get('/formulario', function(req, res){
     res.render("admin/form_add_noticia");
 })
 
-var  rotaNoticias = require('./app/routes/noticias')(app)
+const  rotaNoticias = require('./app/routes/noticias')(app)
 require('./app/routes/home')(app)
 require('./app/routes/formulario_inclusao_noticias')(app)
 
+con.query(`SELECT * FROM noticias`, (err, noticias) => {
 
+    if (err) {
+        reject(err);
+        return;
+    }
+    console.log(noticias)
+   
+})
 
 app.get('/noticia', function(req, res){
     res.render("noticias/noticia");
