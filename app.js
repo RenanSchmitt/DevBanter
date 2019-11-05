f353572a28053409f6ce4bc054b027929ceb224d
Renan Schimitt <c051461@yara.com>
2019-11-05 14:47:09 -0300

    Adiciona o consign

--- app.js
+++ app.js
@@ -1,16 +1,4 @@
-const app = require('./config/server')
-
-app.get('/formulario', function(req, res){
-    res.render("admin/form_add_noticia");
-})
-
-const  rotaPosts = require('./app/routes/posts')(app)
-require('./app/routes/home')(app)
-require('./app/routes/formulario_inclusao_noticias')(app)
-
-app.get('/noticia', function(req, res){
-    res.render("noticias/noticia");
-})
+const app = require('./config/server');
 
 app.listen(3000, function(){
     console.log("Server is online on port 3000")
--- config/server.js
+++ config/server.js
@@ -1,6 +1,10 @@
 var express = require('express');
+const consign= require('consign');
+
 var app = express();
-app.set('view engine', 'ejs')
-app.set('views', './app/views')
+app.set('view engine', 'ejs');
+app.set('views', './app/views');
+
+consign().include('app/routes').into(app);
 
 module .exports = app;
