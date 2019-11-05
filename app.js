d7dfd1624f5df92704663410e7941da8c5eb0b9a
Renan Schimitt <c051461@yara.com>
2019-11-05 11:30:35 -0300

    Add arquivos sql

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const app = require('./config/server')
-const con = require('./config/db')
+
 app.get('/formulario', function(req, res){
     res.render("admin/form_add_noticia");
 })
@@ -7,16 +7,6 @@ app.get('/formulario', function(req, res){
 const  rotaNoticias = require('./app/routes/noticias')(app)
 require('./app/routes/home')(app)
 require('./app/routes/formulario_inclusao_noticias')(app)
-
-con.query(`SELECT * FROM noticias`, (err, noticias) => {
-
-    if (err) {
-        reject(err);
-        return;
-    }
-    console.log(noticias)
-   
-})
 
 app.get('/noticia', function(req, res){
     res.render("noticias/noticia");
