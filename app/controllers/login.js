d2f56df90d4aac6bbf39a13365fa9853cd35a2f8
Renan Schimitt <c051461@yara.com>
2019-11-11 14:28:18 -0300

    eojodwdo

--- app/controllers/login.js
+++ app/controllers/login.js
@@ -3,10 +3,10 @@ module.exports.render_login =  function (app, req, res){
 }
 
 module.exports.validate_login =  function (data, app, req, res){
-    
+    login = req.session;
+  
     if(Object.is(data.email, "admin@gmail.com") && Object.is(data.password, "123")){
-        console.log(data);
-        res.redirect("/posts");
+        res.status(200).redirect("/posts");
     } else {
         res.redirect("/login");     
     }
